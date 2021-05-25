5e48af616bc18b7e37858e896ddcafe530ec36bc
Ismael Rumzan <ismael.rumzan@gmail.com>
2021-05-25 17:30:42 +0300

    connecting to vercel for cms fake

--- pages/info-page/[page].tsx
+++ pages/info-page/[page].tsx
@@ -3,7 +3,7 @@ import { GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import styles from "../../styles/Home.module.css";
 
-function InfoPage({ page }: any) {
+function InfoPage({ data }: any) {
   const router = useRouter();
   // If the page is not yet generated, this will be displayed
   // initially until getStaticProps() finishes running
@@ -14,15 +14,7 @@ function InfoPage({ page }: any) {
     <>
       <div className={styles.container}>
         <main className={styles.main}>
-          <h1 className={styles.title}>This is page: {page.title}</h1>
-          <h2>
-            change the text in <span className={styles.change}>red</span> below
-            in:
-          </h2>
-          <a href="https://github.com/onezero-team/unescaped_characters_error_cms">
-            https://github.com/onezero-team/unescaped_characters_error_cms
-          </a>
-          <p className={styles.change}>{page.text}</p>
+          <h1 className={styles.title}>This is the data: {data.data}</h1>
         </main>
       </div>
     </>
@@ -43,17 +35,20 @@ const redirect404 = () => {
 export const getStaticProps: GetStaticProps = async (context: any) => {
   let { page } = context.params;
   const resp = await fetch(
-    "https://raw.githubusercontent.com/onezero-team/unescaped_characters_error_cms/main/cms.json"
+    "https://nextjs-unescaped-character-cms.vercel.app/cms.json"
+    //"https://raw.githubusercontent.com/onezero-team/unescaped_characters_error_cms/main/cms.json"
   );
   const data = await resp.json();
-  page = data[page];
-  console.log(page);
-  return { props: { page }, revalidate: 1 };
+  console.log(data);
+  //page = data[page];
+  //console.log(page);
+  return { props: { data }, revalidate: 1 };
 };
 
 export async function getStaticPaths(context: any) {
   return {
     paths: [{ params: { page: "אאא_1" } }, { params: { page: "אאא_2" } }],
+    //paths: [{ params: { page: "page1" } }, { params: { page: "page2" } }],
     fallback: true,
   };
 }
